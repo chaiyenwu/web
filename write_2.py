e8e00fb66ec1dcd81d04eff07520310d794cf238
chaiyenwu <wuchaiyen@gmail.com>
2021-09-04 16:47:46 +0800

    Add files via upload

---
+++ write_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: justinwu
+"""
+poem='''呱呱呱呱呱
+醜小鴨呀醜小鴨
+腿兒短短腳掌大
+長長脖子扁嘴巴
+走起路來搖呀搖
+愛到河邊去玩耍
+喉嚨雖小聲音大
+可是只會呱呱呱'''
+print(poem)
+print(len(poem))
+
